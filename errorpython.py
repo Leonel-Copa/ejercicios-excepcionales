d14bb807fc6834eed103edcd108150bffc624c78
gabrielc-act <gcopa.ac@gmail.com>
2025-04-10 23:06:16 -0300

    first commit

---
+++ errorpython.py
@@ -0,0 +1,56 @@
+# Ejercicios de errores y excepciones
+
+#lista = [1,2,3,4]
+
+#print(lista[5])
+
+#diccionario = {'nombre': 'Leo', 'edad': 31}
+
+#print(diccionario['apellido'])
+
+#resultado = 10 / 0
+#print(resultado)
+
+
+#def sumar(a, b):
+    #return a + b
+#resultado = sumar('chau', 7)
+
+
+# Con except y finally
+
+#try:
+   #resultado = 20 / 'leo'
+   #print(resultado)
+#except TypeError:
+   #print(f'Ha ocurrido un error.')
+
+
+#try:
+   #resultado = 10 / 2
+   #print('Ejecutando sin errores \n')
+#except Exception as e:
+   #print(f'Ha ocurrido un error. {e}')
+#else:
+      #print(resultado)  #
+
+#try:
+
+   #resultado = 20 / 5
+   #print(resultado)
+#except:
+   #print('Ha ocurrido un error. {e}')
+#else:
+   #print(resultado)
+#finally:
+   #print('\nMe ejecuto siempre')
+
+try:
+      resultado = 16 * 5
+      print(resultado)
+except:
+   print('Hay un error. {e}')
+else:
+   print(resultado)
+finally:
+   print('Me voy a ejectur siempre')
